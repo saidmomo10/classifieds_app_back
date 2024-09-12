04fbd961a41edf4094c9542b9140079f5c38e0fb
saidmomo10 <saidoumohamef2@gmail.com>
2024-09-12 12:02:51 +0100

    domain

--- config/cors.php
+++ config/cors.php
@@ -30,7 +30,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'https://kaolo-annonce-1.onrender.com'],
+    'allowed_origins' => ['http://localhost:5173', 'https://kaolo-annonce-1.onrender.com', 'https://kaolo-annonce.vercel.app/'],
 
     'allowed_origins_patterns' => [],
 
