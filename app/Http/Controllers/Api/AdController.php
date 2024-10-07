02a5ca9b6ed28c64677bc7c651373137c7bfbaf7
saidmomo10 <saidoumohamef2@gmail.com>
2024-10-07 12:16:56 +0100

    link_2

--- app/Http/Controllers/Api/AdController.php
+++ app/Http/Controllers/Api/AdController.php
@@ -11,18 +11,16 @@ use Illuminate\Support\Facades\Storage;
 
 class AdController extends Controller
 {
-    public function index()
-{
-    $ads = Ad::with('images', 'subcategory', 'user')
-            ->orderBy('created_at', 'desc') // Ordonner par date de création décroissante
-            ->paginate(6);
+    public function index(){
+        $ads = Ad::with('images', 'subcategory', 'user')
+                ->orderBy('created_at', 'desc') // Ordonner par date de création décroissante
+                ->paginate(6);
 
-    return $ads;
-}
+        return $ads;
+    }
 
 
-    public function getAds(Request $request)
-    {
+    public function getAds(Request $request){
         $data = Ad::where('title', 'LIKE','%'.$request->keyword.'%')->with('images', 'subcategory')->orderBy('created_at', 'desc')->get();
         return response()->json($data); 
     }
@@ -45,59 +43,62 @@ class AdController extends Controller
         return response()->json($ads);
     }
 
-    public function search(Request $request)
-    {
+    public function search(Request $request){
         $ads=Ad::where('title',$request->keywords)->get();
-        return response()->json($ads);
-         
+        return response()->json($ads);    
     }
 
-    public function show($id)
-{
-    $ad = Ad::with('images', 'subcategory', 'user')->find($id);
-    $comment = Comment::with('user', 'ad')->where('ad_id', $ad->id)->get();
+    public function show($id){
+        $ad = Ad::with('images', 'subcategory', 'user')->find($id);
+        $comment = Comment::with('user', 'ad')->where('ad_id', $ad->id)->get();
 
-    return ['comment' => $comment, 'ad' => $ad];
-}
+        return ['comment' => $comment, 'ad' => $ad];
+    }
 
     public function store(Request $request){
 
         $user = Auth::user();
         $admin = $user->hasRole('Admin');
-        // $key = $user->subscriptions()->where('status', 'Abonnement actif')->first();
         $key = $user->subscriptions()->where('status', 'Abonnement actif')->latest('activated_at')->first();
         $expire = $user->subscriptions()->where('status', 'Abonnement expire')->latest('activated_at')->first();
-
-        // $key = $user->subscriptions()->latest('activated_at')->first();
-        if($admin){
-            $rules = [
-                'title' => 'required',
-                'description' => 'required',
-                'department' => 'required',
-                'city' => 'required',
-                'delivery_status' => 'required',
-                'state' => 'required',
-                'price_type' => 'required',
-                'phone' => 'required',
-                'user_id' => 'required',
-                'user_subscription_id' => 'required',
-                'subcategory_id' => 'required',
-                'images.*' => [
-                    'required',
-                    'image',
-                    function ($attribute, $value, $fail) {
-                        // Vérifier si une erreur est survenue lors du téléchargement de l'image
-                        if (!$value->isValid()) {
-                            $fail('Une erreur est survenue lors du téléchargement de l\'image.');
-                        }
-                    },
-                ],
-            ];
-
+    
+        $rules = [
+            'title' => 'required',
+            'description' => 'required',
+            'department' => 'required',
+            'city' => 'required',
+            'delivery_status' => 'required',
+            'state' => 'required',
+            'price_type' => 'required',
+            'phone' => 'required',
+            'subcategory_id' => 'required',
+            'images.*' => [
+                'required',
+                'image',
+                function ($attribute, $value, $fail) {
+                    if (!$value->isValid()) {
+                        $fail('Une erreur est survenue lors du téléchargement de l\'image.');
+                    }
+                },
+            ],
+        ];
+    
+        if (!$admin) {
+            $rules['user_subscription_id'] = 'required';
+    
+            if ($key) {
+                $rules['images'][] = 'max:' . $key->max_images;
+            } else if ($expire) {
+                $rules['images'][] = 'max:3';
+            }
+        }
+    
+        $validation = $request->validate($rules);
+    
+        if ($admin) {
             $request->merge(['user_id' => $user->id]);
-            $request->merge(['user_subscription_id' => $key->id]);
-
-            // Créer l'annonce et capturer l'objet créé
+    
+            // Créer l'annonce
             $ad = Ad::create([
                 'title' => $request->title,
                 'price' => $request->price,
@@ -110,141 +111,55 @@ class AdController extends Controller
                 'description' => $request->description,
                 'user_id' => $user->id,
                 'subcategory_id' => $request->subcategory_id,
-                'user_subscription_id' => $key->pivot->id
             ]);
-        
-            // Boucle foreach pour traiter chaque image
+    
             foreach ($request->file('images') as $image) {
-                $imagePath = $image->store('images','public');
-        
-                // Associer chaque image à l'annonce créée
+                $imagePath = $image->store('images', 'public');
                 $ad->images()->create(['path' => $imagePath]);
             }
-        
-            // Retourner l'annonce créée avec le code de statut 201
+    
             return response()->json('Annonce créée avec succès', 201);
-        }else {
-            $rules = [
-                'title' => 'required',
-                'description' => 'required',
-                'department' => 'required',
-                'city' => 'required',
-                'delivery_status' => 'required',
-                'state' => 'required',
-                'price_type' => 'required',
-                'phone' => 'required',
-                'user_id' => 'required',
-                'user_subscription_id' => 'required',
-                'subcategory_id' => 'required',
-                'images.*' => [
-                    'required',
-                    'image',
-                    function ($attribute, $value, $fail) {
-                        // Vérifier si une erreur est survenue lors du téléchargement de l'image
-                        if (!$value->isValid()) {
-                            $fail('Une erreur est survenue lors du téléchargement de l\'image.');
+        } else {
+            // Logique pour les utilisateurs normaux (avec abonnement)
+            if ($key && $key->pivot) {
+                if ($key->pivot) {
+                    $use = Ad::where('user_subscription_id', $key->pivot->id)->count();
+    
+                    if ($use >= $key->max_ads) {
+                        return response()->json(['message' => 'Vous avez épuisé votre quota'], 404);
+                    } else {
+                        $request->merge(['user_id' => $user->id, 'user_subscription_id' => $key->id]);
+    
+                        $ad = Ad::create([
+                            'title' => $request->title,
+                            'price' => $request->price,
+                            'phone' => $request->phone,
+                            'price_type' => $request->price_type,
+                            'city' => $request->city,
+                            'delivery_status' => $request->delivery_status,
+                            'state' => $request->state,
+                            'description' => $request->description,
+                            'user_id' => $user->id,
+                            'subcategory_id' => $request->subcategory_id,
+                            'user_subscription_id' => $key->pivot->id,
+                        ]);
+    
+                        foreach ($request->file('images') as $image) {
+                            $imagePath = $image->store('images', 'public');
+                            $ad->images()->create(['path' => $imagePath]);
                         }
-                    },
-                ],
-            ];
-            
-            // Ajouter une règle de validation pour limiter le nombre total d'images
-            if ($key) {
-                $rules['images'][] = 'max:' . $key->max_images;
-            } else if($expire){
-                $rules['images'][] = 'max:3';
-            }
-            
-            $validation = $request->validate($rules);
-     
-            //  $mainly_picture = null;
-            //  $secondary_picture = null;
-            //  $tertiary_picture = null;
-        
-            //  if ($request->hasFile('image')) {
-            //      if ($request->file('image')[0]) {
-            //          $mainly_picture = Storage::disk()->put('mainly_pictures', $request->file('image')[0]);
-            //      }
-            //      if ($request->file('image')[1]) {
-            //          $secondary_picture = Storage::disk()->put('secondary_pictures', $request->file('image')[1]);
-            //      }
-            //      if ($request->file('image')[2]) {
-            //          $tertiary_picture = Storage::disk()->put('tertiary_pictures', $request->file('image')[2]);
-            //      }
-            //  }
     
-            // $mainly_picture = Storage::disk()->put('mainly_pictures', $request->file('image1'));
-            // $secondary_picture = Storage::disk()->put('secondary_pictures', $request->file('image2'));
-            // $tertiary_picture = Storage::disk()->put('tertiary_pictures', $request->file('image3'));
-     
-             //dd($picture);
-    
-            if ($key) {
-                if (Auth::check()) {
-    
-                    if($key->pivot){
-                        $use = Ad::where('user_subscription_id', $key->pivot->id)->count();
-    
-                        if($use >= $key->max_ads){
-                            return response()->json(['message' => 'Vous avez épuisé votre quota'], 404);
-                        }else{
-                            $request->merge(['user_id' => $user->id]);
-                            $request->merge(['user_subscription_id' => $key->id]);
-        
-                            // Créer l'annonce et capturer l'objet créé
-                            $ad = Ad::create([
-                                'title' => $request->title,
-                                'price' => $request->price,
-                                'phone' => $request->phone,
-                                'price_type' => $request->price_type,
-                                'city' => $request->city,
-                                'delivery_status' => $request->delivery_status,
-                                'state' => $request->state,
-                                'description' => $request->description,
-                                'user_id' => $user->id,
-                                'subcategory_id' => $request->subcategory_id,
-                                'user_subscription_id' => $key->pivot->id
-                            ]);
-                        
-                            // Boucle foreach pour traiter chaque image
-                            foreach ($request->file('images') as $image) {
-                                $imagePath = $image->store('images','public');
-                        
-                                // Associer chaque image à l'annonce créée
-                                $ad->images()->create(['path' => $imagePath]);
-                            }
-                        
-                            // Retourner l'annonce créée avec le code de statut 201
-                            return response()->json('Annonce créée avec succès', 201);
-                        }
-                    }else{
-                        return response()->json(['message' => 'Aucun abonnement'], 404);
+                        return response()->json('Annonce créée avec succès', 201);
                     }
-                    
-    
-    
-    
-                    
                 } else {
-                    // Retourner une réponse d'erreur si l'utilisateur n'est pas authentifié
-                    return response()->json("erreur");
+                    return response()->json(['message' => 'Aucun abonnement'], 404);
                 }
-            }else if($expire || $expire->pivot == null){
-                return response()->json("Aucun abonnement");
+            } else{
+                return response()->json(['message' => 'Aucun abonnement'], 404);
             }
         }
-
-
-        
-
-
-         
-        
-         
-
-         //dd($request);
-         
     }
+    
 
     public function update($id, Request $request){
         // Récupérer l'utilisateur authentifié
