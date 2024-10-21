b7e3f007b2a06341271cd118ce0ca278f83c1b82
saidmomo10 <saidoumohamef2@gmail.com>
2024-10-21 15:48:19 +0100

    migration_5

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,20 @@
-# #!/bin/sh
+# #!/bin/bash
 
-# # Attendre que la base de données soit disponible
-# echo "Waiting for database connection..."
-# until php artisan migrate:status > /dev/null 2>&1; do
-#   echo "Waiting for PostgreSQL to be ready..."
-#   sleep 3
+# # Attendre que la base de données soit disponible avant de lancer les migrations
+# until php -r "new PDO('mysql:host=${DB_HOST};dbname=${DB_DATABASE}', '${DB_USERNAME}', '${DB_PASSWORD}');" 2>/dev/null; do
+#     echo "Waiting for MySQL database connection..."
+#     sleep 5php artisan migrate:reset --force
 # done
 
-# # Exécuter les migrations
-# php artisan migrate --force
+# # Lancer les migrations
 
-# # Démarrer l'application
+# php artisan migrate --force
+# php artisan storage:link
+
+# RUN php artisan db:seed --class=PermissionTableSeeder
+# RUN php artisan db:seed --class=CategoryTableSeeder
+# RUN php artisan db:seed --class=SubCategoryTableSeeder
+# RUN php artisan db:seed --class=CreateAdminUserSeeder
+
+# # Exécuter la commande donnée
 # exec "$@"
